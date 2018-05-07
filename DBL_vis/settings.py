9e5e793a41777e82d76e40fd8cbf96dde13c7fa7
Leonardo <leonardo.mathon@hotmail.com>
2018-05-07 13:30:43 +0200

    Added a virtualenviroment and changed database settings

--- DBL_vis/settings.py
+++ DBL_vis/settings.py
@@ -75,10 +75,20 @@ WSGI_APPLICATION = 'DBL_vis.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dblwebtech',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
---
+++ README.md
@@ -0,0 +1,7 @@
+# 2IOA0 DBL HTI + Webtech #
+
+## Admin login ##
+Use /admin to get to the admin panel.
+Username      | Password
+------------- | -------------
+admin         | 2IOA0
