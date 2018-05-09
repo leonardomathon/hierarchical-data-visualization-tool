5a129a6cf3f04b3bf52af54c88933b4ff8595074
Matthijs Logemann <matthijs@logicbit.nl>
2018-05-09 12:38:37 +0200

    Update static file path for server

--- DBL_vis/settings.py
+++ DBL_vis/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "assets")
+STATIC_ROOT = '/app/assets/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
