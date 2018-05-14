01a3f466fad1874f366e7f28070fc8416f840f8c
Matthijs Logemann <matthijs@logicbit.nl>
2018-05-14 12:12:40 +0200

    Add upload field to homepage

--- DBL_vis/urls.py
+++ DBL_vis/urls.py
@@ -17,7 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from home.views import HomePageView, PaginationView, FormWithFilesView, MiscView
+from home.views import HomePageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
