2797348c0a50091e09f160e46e4cdb5ebefe24ac
Leonardo <leonardo.mathon@hotmail.com>
2018-05-14 17:50:14 +0200

    Uploaded dataset can be read, returns HTML or JSON file

--- display/urls.py
+++ display/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^(?P<data_id>[0-9]+)/$', views.display, name='display'),
+    url(r'^(?P<data_id>[0-9]+)/read$', views.read, name='display'),
+    url(r'^(?P<data_id>[0-9]+)/json$', views.returnJson, name='display'),
 ]
--- display/views.py
+++ display/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from upload.models import Data
+from django.conf import settings
+import json
+
+from django.http import HttpResponse
 
 def index(request):
     context = {
@@ -10,3 +14,33 @@ def index(request):
 def display(request, data_id):
     data = get_object_or_404(Data, pk=data_id)
     return render(request, 'display/display.html', {'data': data})
+
+
+def read(request, data_id):
+    datafile = []
+    data = get_object_or_404(Data, pk=data_id)
+    file = open(settings.BASE_DIR + data.file_path.url, "r")
+    for line in file:
+        datafile.append(line)
+    if len(datafile) > 1:
+        datafile.clear()
+        datafile.append('Sorry, this dataset is too big to be displayed...')
+    context = {
+        'file': datafile
+    }
+    return render(request, 'display/read.html', context)
+
+def returnJson(request, data_id):
+    datafile = []
+    data = get_object_or_404(Data, pk=data_id)
+    file = open(settings.BASE_DIR + data.file_path.url, "r")
+    for line in file:
+            datafile.append(line)
+    if len(datafile) > 1:
+        datafile.clear()
+        datafile.append('Sorry, this dataset is too big to be displayed...')
+    context = {
+            'file': datafile
+        }
+
+    return HttpResponse(json.dumps(context), content_type="application/json")
