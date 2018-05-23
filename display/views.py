5d74e355bebed939e56eed43cc7429645063b9e3
Leonardo <leonardo.mathon@hotmail.com>
2018-05-23 10:56:35 +0200

    Handled invalid tree exception

--- display/views.py
+++ display/views.py
@@ -39,33 +39,41 @@ def returnJson(request, data_id):
     return HttpResponse(open(settings.BASE_DIR + "/uploads/data_"+data.dataname+"/"+data.dataname+".json", "r"), content_type="application/json")
 
 
+def get_json(node):
+    node.name = node.name.replace("'", '')
+
+    #If node has no name, assign random name
+    if node.name == "":
+        node.name = random.randint(0, 10000)
+
+    # JSON format
+    json = {"name": node.name,
+            "type": "node" if node.children else "leaf",
+            "branch_length": str(node.dist),
+            }
+
+    # Get children
+    if node.children:
+        json["children"] = []
+        for ch in node.children:
+            json["children"].append(get_json(ch))
+
+    return json
+
 def generate_json(data_id):
     data = get_object_or_404(Data, pk=data_id)
     if os.path.isfile(settings.BASE_DIR + "/uploads/data_"+data.dataname+"/"+data.dataname+".json"):
         return 0
     else:
         data_name = data.dataname
-        t = Tree(settings.BASE_DIR + data.file_path.url, format=1)
-        f= open(settings.BASE_DIR + "/uploads/data_"+data.dataname+"/"+data.dataname+".json","w+")
-        f.write(str(get_json(t)).replace("'", '"'))
+        try:
+            t = Tree(settings.BASE_DIR + data.file_path.url, format=1)
+            f= open(settings.BASE_DIR + "/uploads/data_"+data.dataname+"/"+data.dataname+".json","w+")
+            f.write(str(get_json(t)).replace("'", '"'))
+        except:
+            f = open(settings.BASE_DIR + "/uploads/data_" +
+                     data.dataname+"/"+data.dataname+".json", "w+")
+            f.write("Invalid tree")
 
-def get_json(node):
-    node.name = node.name.replace("'", '')
 
-    #If node has no name, assign random name
-    if node.name == "":
-        node.name = random.randint(0,10000)
-        
-    # JSON format
-    json = { "name": node.name, 
-             "type": "node" if node.children else "leaf",
-             "branch_length": str(node.dist),
-             }  
-    
-    # Get children
-    if node.children:
-        json["children"] = []
-        for ch in node.children:
-            json["children"].append(get_json(ch))
-    return json
 
