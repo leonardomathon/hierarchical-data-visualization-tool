0c81bea791d69b7571543e5cdb4382ac7ceb8a44
Leonardo <leonardo.mathon@hotmail.com>
2018-05-25 00:19:08 +0200

    Searching is added + minor changes frontend

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^(?P<data_id>[0-9]+)/results$', views.results, name='results'),
 ]
--- search/views.py
+++ search/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render, get_object_or_404
 from upload.models import Data, user_directory_path
 from django.conf import settings
 from django.http import HttpResponse
+from django import forms
+
+from upload.views import Data
 
 def index(request):
+    data = Data.objects.all()
     context = {
-        'data': Data.objects.all().order_by('-pk')
+        'data': data
     }
     return render(request, 'search/index.html', context)
-
-def results(request, name):
-    context = {
-        'data': Data.objects.all().order_by('dataname')
-    }
-    return render(request, 'search/results.html', context)
